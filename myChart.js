ea32433b22e5ddd5585df01c35aaa7fc21f537b5
Navarrete <juan.navarrete@meggitt.com>
2019-04-16 10:19:51 -0700

    Add several types of charts

--- myChart.js
+++ myChart.js
@@ -1,17 +1,58 @@
+function float2dollar(value){
+    return "U$ "+(value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
 function renderChart(data, labels) {
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: labels,
-            datasets: [{
-                label: 'This week',
-                data: data,
-            }]
+            datasets: [
+                {
+                    label: 'This week',
+                    data: [
+                        20000,
+                        14000,
+                        12000,
+                        15000,
+                        18000,
+                        19000,
+                        22000
+                    ],
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                },
+                {
+                    label: 'Last week',
+                    data: [
+                        19000,
+                        10000,
+                        14000,
+                        14000,
+                        15000,
+                        22000,
+                        24000
+                    ],
+                    borderColor: 'rgba(192, 192, 192, 1)',
+                    backgroundColor: 'rgba(192, 192, 192, 0.2)',
+                }
+            ]
         },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function (value, index, values) {
+                            return float2dollar(value);
+                        }
+                    }
+                }]
+            },
+        }
     });
 }
-
 $("#renderBtn").click(
     function () {
         data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
